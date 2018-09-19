a4100b12204480400da5a7f0634c89a78f845af0
Evgeniy <43406970+EvgeniyNazarov@users.noreply.github.com>
2018-09-19 20:04:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,20 @@
-// Author Evgeniy Nazarov
-
-// Second Epam training task 
-
-// Create traffic light with output color in the console.
-
-// You input minute(from 0 to n) and get which light fires on the traffic light.
-
-// First two minutes red light, after three minutes yellow light and five minutes green light.
-
-// Class Main start the program.
+/* Author Evgeniy Nazarov
+   Second Epam training task 
+   Create traffic light with output color in the console.
+   You input minute(from 0 to n) and get which light fires on the traffic light.
+   First two minutes red light, after three minutes yellow light and five minutes green light.
+   Class Main start the program. */
 
 
 package com.trafficLightApp;
-
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        
         Scanner in = new Scanner(System.in);
-        
         Lights light = new Lights();
-        
         TrafficLightMethods tlm = new TrafficLightMethods();
-        
         int minute = tlm.getMinute(in);
-        
         tlm.CheckLight(light.getRed(), light.getYellow(), light.getGreen(), minute);
-        
-   }
-    
-}
+    }
+ } 
