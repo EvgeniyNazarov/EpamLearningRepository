8e3f487e8ef0e9d25397e1608a52f7f063358fcf
Evgeniy <43406970+EvgeniyNazarov@users.noreply.github.com>
2018-09-21 17:23:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@
  * Create traffic light with output color in the console.
  * You input minute(from 0 to n) and get which light fires on the traffic light.
  * First two minutes red light, after three minutes yellow light and five minutes green light.*/
-
 package com.trafficLightApp;
 
 import java.util.Scanner;
@@ -13,7 +12,6 @@ import java.util.Scanner;
  * @version 1.0 18 Sep 2018  * @author
  * Evgeniy Nazarov
  */
-
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
